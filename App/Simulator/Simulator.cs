5bff37056dd24b0da7c1e3f038c7037367d33ba8
mireaandy <mireaandy@gmail.com>
2019-03-29 19:14:07 +0200

    Update per total.
    In urma schimbarii simulatorului, s-au efectuat schimbari de-a lungul proiectului.

---
+++ App/Simulator/Simulator.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+
+    /// <summary>
+    /// Enumerare folosita pentru a transmite/receptiona comenzi
+    /// </summary>
+    public enum Command
+    {
+        Stopped = 0,
+        Started = 1,
+        Case_1 = 2,
+        Case_2 = 4,
+        Case_3 = 8,
+        Case_4 = 16
+    }
+
+    /// <summary>
+    /// Enumerare folosita pentru a modela starea procesului
+    /// </summary>
+    public enum ProcessState
+    {        
+        PumpOne = 1,
+        PumpTwo = 2,
+        PumpThree=4,
+        Valve = 8,
+        Level_1 = 16,
+        Level_2 = 32,
+        Level_3 = 64,
+        Stopped = 128
+    }
+
+
+
+    public class Simulator
+    {
+        private byte[] _state = new byte[] { 0x00, 0x00};
+        
+        private void Start(int Delay)
+        {
+            System.Threading.Thread.Sleep(Delay);
+            _state[0] = (int)ProcessState.Level_1
+            | (int)ProcessState.Valve;
+            System.Threading.Thread.Sleep(Delay);
+
+            _state[0] = (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2
+                | (int)ProcessState.Valve;
+            System.Threading.Thread.Sleep(Delay);
+
+            _state[0] = (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2
+                | (int)ProcessState.PumpOne
+                | (int)ProcessState.Valve;
+            System.Threading.Thread.Sleep(Delay);
+        }
+
+        private void Stop(int Delay, int opt)
+        {
+            switch (opt)
+            {
+                case 1:
+                {
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0]= (int)ProcessState.Stopped
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    break;
+                }
+                case 2:
+                {
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.Stopped
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    break;
+                }
+                case 3:
+                {
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpThree
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpThree
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpThree
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.Stopped
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                        break;
+                }
+                case 4:
+                {
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2
+                | (int)ProcessState.Level_3
+                | (int)ProcessState.Valve;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.PumpTwo
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2
+                | (int)ProcessState.Level_3;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.PumpTwo
+                | (int)ProcessState.Level_1
+                | (int)ProcessState.Level_2;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.PumpTwo
+                | (int)ProcessState.Level_1;      
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.PumpOne
+                | (int)ProcessState.PumpTwo;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    _state[0] = (int)ProcessState.Stopped;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    break;
+                }
+                default:
+                {
+                    _state[0] = (int)ProcessState.Stopped;
+                    System.Threading.Thread.Sleep(Delay);
+
+                    break;
+                }
+            }
+
+        }  
+
+        public void UpdateState(int CommandState)
+        {            
+            if(CommandState == (int)Command.Started)
+            {
+                Start(5000);
+            }
+            else
+            {
+                switch(CommandState)
+                {
+                    case ((int)Command.Stopped | (int)Command.Case_1):
+                        Stop(5000, 1);
+                        break;
+                    case ((int)Command.Stopped | (int)Command.Case_2):
+                        Stop(5000, 2);
+                        break;
+                    case ((int)Command.Stopped | (int)Command.Case_3):
+                        Stop(5000, 3);
+                        break;
+                    case ((int)Command.Stopped | (int)Command.Case_4):
+                        Stop(5000, 4);
+                        break;
+                }
+
+            }            
+        }
+
+        public byte[] GetState()
+        {
+            return _state;
+        }
+    }
+}
---
+++ App/TCPListenerTestApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCP_PLC
+{
+    /// <summary>
+    /// Program  folosit  pentru a simula functionarea automatului.
+    /// Exista o clasa Sender care trimite date catre apliatia de monitorizare
+    /// si o clasa Listner care primeste comenzi de la aplicatia de monitorizare
+    /// partea de comunicare cu aplicatia de monitorizare trebuie implementata in paralel
+    /// existand un thread separat pentru transmiterea de date si un thread separat pentru receptionarea de date
+    /// </summary>
+    public class Program
+	{        
+        static BackgroundWorker senderWorker;	
+        static Simulator.Simulator process;
+
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Press enter to start...");
+            Console.ReadLine();
+
+            process = new Simulator.Simulator();
+
+
+            ///pentru partea de transmitere de date am folosit un background worker 
+            ///care poate sa ruleze o metoda intr-un thread separat (metoda rulata este SenderWorker_DoWork)
+            ///atentie la partea de update a UI-ului, pentru a face update la UI din background worker se foloseste
+            ///SenderWorker_ProgressChanged, daca nu folosit aceasta metoda se poate sa primiti exceptii legate de
+            ///faptul ca incercati sa faceti update pe UI dintr-un alt thread decat cel principal
+            senderWorker = new BackgroundWorker();
+			senderWorker.WorkerReportsProgress = true;
+			senderWorker.DoWork += SenderWorker_DoWork;
+			senderWorker.ProgressChanged += SenderWorker_ProgressChanged;
+			senderWorker.RunWorkerAsync();
+
+            /// varianta mai noua de implementare a paralelismului utilizand TPL
+            /// este recomandat sa utilizati aceasta varianta, este mai usor de gestionat si ofera mai multe facilitati
+            /// mai multe informatii si exemple: https://www.codeproject.com/Articles/1083787/Tasks-and-Task-Parallel-Library-TPL-Multi-threadin
+            Thread listenThread = new Thread(Listen);
+            listenThread.Start();
+		}
+
+		private static void SenderWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			Console.WriteLine(e.UserState);
+		}
+				
+		private static void SenderWorker_DoWork(object sender, DoWorkEventArgs e)
+		{
+			Sender dataSender = new Sender(senderWorker, process);
+			dataSender.Send();
+		}
+
+        private static void Listen()
+        {
+            Listener listener = new Listener(process);
+            listener.Listen();
+        }
+	}
+}
